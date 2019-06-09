c575a391230b24e53e7f1617f3e52d1a01b0b306
JoaoR <jppribeiro@gmail.com>
2019-06-09 12:39:51 +0100

    array

--- src/org/academiadecodigo/batmancave/maze/Maze.java
+++ src/org/academiadecodigo/batmancave/maze/Maze.java
@@ -1,4 +1,19 @@
 package org.academiadecodigo.batmancave.maze;
 
+import java.lang.reflect.Array;
+
 public class Maze {
+
+    private Array[] layout;
+
+    public Maze(int cols, int rows) {
+
+        layout = new Array[cols];
+
+        for(int i = 0; i < layout.length; i++) {
+            
+        }
+
+    }
+
 }
