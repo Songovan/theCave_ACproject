e37943567c53049ae8c28014922a9a3bba39aba6
Songovan <tiago_740@hotmail.com>
2019-06-12 03:38:04 +0100

    added resources to make menu

---
+++ src/org/academiadecodigo/batmancave/Menu.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.batmancave;
+
+import org.academiadecodigo.simplegraphics.mouse.MouseEvent;
+import org.academiadecodigo.simplegraphics.mouse.MouseHandler;
+
+public class Menu implements MouseHandler {
+
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+    }
+}
